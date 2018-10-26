877b06206b92b802477ec61eee30063f36f034f9
张传奇 <zhangchuanqi@okay.cn>
2018-10-26 21:23:37 +0800

    Initial Commit

---
+++ newCarveOut/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newCarveOut
+//
+//  Created by 张传奇 on 2018/10/26.
+//  Copyright © 2018 张传奇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newCarveOut/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  newCarveOut
+//
+//  Created by 张传奇 on 2018/10/26.
+//  Copyright © 2018 张传奇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
